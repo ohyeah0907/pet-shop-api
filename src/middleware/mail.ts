d63dd10f1ca92eaecbc16f13f7ccd63412a4240d
ohyeah0907 <phamductri2001@gmail.com>
2023-12-11 22:18:30 +0700

    fix(api): edit url of mail verification

--- src/middleware/mail.ts
+++ src/middleware/mail.ts
@@ -3,25 +3,26 @@ import { emailConstant } from "../constants/mail";
 
 const transport = nodemailer.createTransport(emailConstant);
 
-export const sendMailVerification = async (email: string, token: string): Promise<Boolean> => {
-    console.log(email, token)
-    const mailOptions = {
-        from: emailConstant.auth!.user,
-        to: email,
-        subject: "Verify your email",
-        html: `<p>Click <a href="http://localhost:4000/api/auth/verify-email/${token}">here</a> to verify your email</p>`,
-    };
+export const sendMailVerification = async (
+  email: string,
+  token: string,
+): Promise<Boolean> => {
+  console.log(email, token);
+  const mailOptions = {
+    from: emailConstant.auth!.user,
+    to: email,
+    subject: "Verify your email",
+    html: `<p>Click <a href="https://pet-shop-service.onrender.com/api/auth/verify-email/${token}">here</a> to verify your email</p>`,
+  };
 
-    return transport
-        .sendMail(mailOptions)
-        .then((info) => {
-            console.log(info);
-            return true;
-        })
-        .catch((error) => {
-            console.log(error);
-            return false;
-        });
-
+  return transport
+    .sendMail(mailOptions)
+    .then((info) => {
+      console.log(info);
+      return true;
+    })
+    .catch((error) => {
+      console.log(error);
+      return false;
+    });
 };
-
